6fd86c34f8b7edd7472d97f3322ade05f313c9e6
Benny Boon <bennybwkrap@gmail.com>
2019-10-30 22:00:02 +0800

    Update chat_id

--- assets/js/email.js
+++ assets/js/email.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 		} else {
 			event.preventDefault();
 			var $botToken = '992849472:AAHZZ3jEbcGh_TxUX3vvnJMwvmbTuMmQXik';
-			$('#tgMsgFrame').prop('src', 'https://api.telegram.org/bot' + $botToken + '/sendMessage?chat_id=-373170216&text=Enquiry from ' + $("input[name='nameTB']").val() + '%0D%0AEmail: ' + $("input[name='emailTB']").val() + '%0D%0AEnquiry Type: ' + $("input[name='enquiryType']").val() + '%0D%0A%0D%0AMessage:%0D%0A' + $("#messageTB").val().replace(/\n\r?/g, '%0D%0A') );
+			$('#tgMsgFrame').prop('src', 'https://api.telegram.org/bot' + $botToken + '/sendMessage?chat_id=-365041799&text=Enquiry from ' + $("input[name='nameTB']").val() + '%0D%0AEmail: ' + $("input[name='emailTB']").val() + '%0D%0AEnquiry Type: ' + $("input[name='enquiryType']").val() + '%0D%0A%0D%0AMessage:%0D%0A' + $("#messageTB").val().replace(/\n\r?/g, '%0D%0A') );
 			alert("Your enquiry has been submitted!");
 		}
     });
