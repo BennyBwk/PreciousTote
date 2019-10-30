0227b1fc73da5fb1797830aa00b28b39b0c9da54
Benny Boon <bennybwkrap@gmail.com>
2019-10-30 21:58:46 +0800

    Draft Launch

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
     $('#contactForm').submit(function(event) {
-        if ($( "#langDiv" ).html() == "EN") {
-            if ($("input[name='nameTB']").val().length === 0 || !$.trim($("input[name='nameTB']").val()) ) {
-                event.preventDefault();
-                alert("Please fill in your name!");
-            } else if ($("input[name='emailTB']").val().length === 0 || !$.trim($("input[name='emailTB']").val()) ) {
-                event.preventDefault();
-                alert("Please fill in your email address!");
-            } else if ($("input[name='enquiryType']").val().length === 0 ) {
-                event.preventDefault();
-                alert("Please select an enquiry type!");
-            } else if (!$.trim($("#enquriyTB").val())) {
-                event.preventDefault();
-                alert("Your message is empty!");
-            }
-        }
+		if ($("input[name='nameTB']").val().length === 0 || !$.trim($("input[name='nameTB']").val()) ) {
+			event.preventDefault();
+			alert("Please fill in your name!");
+		} else if ($("input[name='emailTB']").val().length === 0 || !$.trim($("input[name='emailTB']").val()) ) {
+			event.preventDefault();
+			alert("Please fill in your email address!");
+		} else if ($("input[name='enquiryType']").val().length === 0 ) {
+			event.preventDefault();
+			alert("Please select an enquiry type!");
+		} else if (!$.trim($("#messageTB").val())) {
+			event.preventDefault();
+			alert("Your message is empty!");
+		} else {
+			event.preventDefault();
+			var $botToken = '992849472:AAHZZ3jEbcGh_TxUX3vvnJMwvmbTuMmQXik';
+			$('#tgMsgFrame').prop('src', 'https://api.telegram.org/bot' + $botToken + '/sendMessage?chat_id=-373170216&text=Enquiry from ' + $("input[name='nameTB']").val() + '%0D%0AEmail: ' + $("input[name='emailTB']").val() + '%0D%0AEnquiry Type: ' + $("input[name='enquiryType']").val() + '%0D%0A%0D%0AMessage:%0D%0A' + $("#messageTB").val().replace(/\n\r?/g, '%0D%0A') );
+			alert("Your enquiry has been submitted!");
+		}
     });
 });
